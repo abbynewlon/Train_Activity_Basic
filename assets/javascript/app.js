e50eb6858e06840754a5e680928812fee758455a
Your Name <you@example.com>
2019-04-26 07:57:58 -0700

    Initialize Firebase and captured the click events

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-// Initialize Firebase
+$(document).ready(function () {
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyDcRACiXiWcOENQyID29GuDW2P6l6-YBwY",
     authDomain: "train-activity-9f70f.firebaseapp.com",
@@ -7,7 +8,16 @@
     storageBucket: "train-activity-9f70f.appspot.com",
     messagingSenderId: "1098310526369"
   };
-  //firebase.initializeApp(config);
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  //initailize varibles. should somehow be in an object or something
+  var trainName;
+  var destination;
+  var frequency;
+  var nextArrival;
+  var minutesAway;
 
 
   //testing moment.js
@@ -16,3 +26,18 @@
   var result = moment(timer).format(format);
   console.log(result);
 
+  //getting user input from add train form
+
+  $("#new-train-info").on("click", function(event) {
+    event.preventDefault();
+  
+    var valueTrainName = $("#trainNameInput").val().trim();
+    var valueDestinationName = $("#destinationNameInput").val().trim();
+    //need to change whole moment thing here
+    var valueFirstTrain = moment($("#firstTrainInput").val().trim(), "MM/DD/YYYY").format("X");
+    var valueFrequency = $("#frequencyInput").val().trim();
+    alert("into the click");
+  });
+
+  
+});
